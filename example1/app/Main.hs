4993b6e0a4654e4d7550357c5e352093d5b78ba0
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2025-01-06 18:47:14 +0100

    Switch to old style multiline string

--- example1/app/Main.hs
+++ example1/app/Main.hs
@@ -1,11 +1,19 @@
-{-# LANGUAGE MultilineStrings #-}
 module Main where
 
 import Data.IORef
 import GHC.JS.Foreign.Callback
 import GHC.JS.Prim
 
-foreign import javascript unsafe "install_handler"
+foreign import javascript unsafe 
+  " ((f) => { \
+    \var node = document.createElement(\"button\"); \
+    \node.textContent = \"Click me!\"; \
+    \node.onclick = function () { \
+    \  var result = f(); \
+    \  console.log(\"Clicked \" + result + \" times\"); \
+    \}; \
+    \document.body.appendChild(node); \
+  \})"
   install_handler :: Callback (IO JSVal) -> IO ()
 
 main :: IO ()
--- example1/install_handler.js
+++
@@ -1,9 +0,0 @@
-function install_handler (f) {
-  var node = document.createElement("button");
-  node.textContent = "Click me!";
-  node.onclick = function () {
-    var result = f();
-    console.log("Clicked " + result + " times");
-  };
-  document.body.appendChild(node);
-}
